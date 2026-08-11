7703f5fa5aabe02ecc2e47535c1dbd94330f6de8
bomin <utmmyppol@unionsystems.co.kr>
2026-08-11 17:06:26 +0900

    chore: V27, V28 마이그레이션 파일 제거 (ddl.sql + seed.sql에 병합 완료)

--- sql/V28__add_inquiry_file_url.sql
+++
@@ -1,3 +0,0 @@
--- 문의 첨부파일 URL 컬럼 추가
-ALTER TABLE union_schema.inquiries ADD COLUMN IF NOT EXISTS file_url VARCHAR(500);
-ALTER TABLE dataware_schema.inquiries ADD COLUMN IF NOT EXISTS file_url VARCHAR(500);
