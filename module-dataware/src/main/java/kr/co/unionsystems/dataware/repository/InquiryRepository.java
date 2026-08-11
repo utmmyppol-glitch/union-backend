3291c88b88f80222aad1435a20f2e62726eb16c5
bomin <utmmyppol@unionsystems.co.kr>
2026-08-11 13:19:59 +0900

    chore: InquiryRepository 미사용 import 제거

--- module-dataware/src/main/java/kr/co/unionsystems/dataware/repository/InquiryRepository.java
+++ module-dataware/src/main/java/kr/co/unionsystems/dataware/repository/InquiryRepository.java
@@ -1,13 +1,13 @@
 package kr.co.unionsystems.dataware.repository;
 
 import kr.co.unionsystems.dataware.entity.Inquiry;
-import kr.co.unionsystems.common.entity.BaseInquiry.InquiryStatus;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository("datawareInquiryRepository")
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
